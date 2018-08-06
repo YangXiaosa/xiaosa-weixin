3b0bbfcaa7d715e28dcbf82900ef044a977a64a3
yangxiaosa <15931082031@163.com>
2018-08-06 09:26:54 +0800

    增加接收微信消息

--- src/main/java/com/xiaosa/wx/controller/WxAuthController.java
+++ src/main/java/com/xiaosa/wx/controller/WxAuthController.java
@@ -1,13 +1,18 @@
 package com.xiaosa.wx.controller;
 
+import com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderImpl;
 import com.xiaosa.wx.service.WxService;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -35,6 +40,32 @@ public class WxAuthController {
         }
         return echostr;
     }
+
+    @ResponseBody
+    @PostMapping(produces = "application/xml; charset=UTF-8")
+    public String post(HttpServletRequest request,@RequestBody String requestBody, @RequestParam("signature") String signature,
+                       @RequestParam(name = "encrypt_type", required = false) String encType,
+                       @RequestParam(name = "msg_signature", required = false) String msgSignature,
+                       @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce) throws Exception {
+        if (!checkSignature(signature, timestamp, nonce, token)) {
+            throw new IllegalArgumentException("非法请求，可能属于伪造的请求！");
+        }
+        System.out.println(requestBody);
+        String result = "<![CDATA[爱你 ]]>";
+        String toUser = null;
+        try {
+            String content = requestBody.subSequence(requestBody.indexOf("<Content>"),requestBody.indexOf("</Content>")).toString().replace("<Content>","");
+            if (content.equals("<![CDATA[老公]]>")) {
+                result = "<![CDATA[宝宝，I LOVE YOU ]]>";
+            }
+            toUser  = requestBody.subSequence(requestBody.indexOf("<FromUserName>"),requestBody.indexOf("</FromUserName>")).toString().replace("<FromUserName>","");
+        } catch (Exception e) {
+            result = "<![CDATA[回复老公，得惊喜]]>";
+        }
+
+        return "<xml> <ToUserName>"+toUser+"</ToUserName> <FromUserName><![CDATA[gh_2a542f784315]]></FromUserName> <CreateTime>12345678</CreateTime> <MsgType><![CDATA[text]]></MsgType> <Content>"+result+"</Content> </xml>";
+    }
+
 
     private boolean checkSignature(String signature, String timestmp, String nonce, String token) {
         List<String> params = Arrays.asList(timestmp,nonce,token);
